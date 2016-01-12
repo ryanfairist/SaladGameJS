e8d5c56be1e2cb2d60fed367d61ee021e0e75810
RyanFairist <ryanfairistrichard@gmail.com>
2016-01-11 22:10:08 -0500

    add and style font, toggle pictures and recipe. fix spelling of recipe id. add body background color. style buttons. add animation to shake play button.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
+
+	$(document).ready(function(){
 	$('.style').click(function() 
 		{$(this).next().toggle(); 
-			$(this).find('.receipe');//.toggleClass('glyphicon-minus'); this is if you have an image you would like to toggle witht he button/thing that was clicked
+			$(this).find('.recipe');//.toggleClass('glyphicon-minus'); this is if you have an image you would like to toggle witht he button/thing that was clicked
 			 });
 });
+			  
